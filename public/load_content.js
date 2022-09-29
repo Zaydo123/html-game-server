642c3a0628576246b80d1f89def71094199748ee
Zayd Alzein <zaydalzein@gmail.com>
2022-09-29 18:14:06 -0500

    sort popularity

--- public/load_content.js
+++ public/load_content.js
@@ -7,10 +7,19 @@ function getVisits(){
             success:function(data){
                 resolve(data);
                 document.getElementById('visitorsCounter').innerHTML = "Total Visits : "+data.main.visitors;
+                //sort all in data.main.apps by visits
+                data.main.apps.sort((a,b)=>{
+                    return a.visits - b.visits;
+                });
+                for(let i=0;i<data.main.apps.length;i++){
+                    if(document.getElementById(data.main.apps[i].route_name)!=null){
+                        console.log(document.getElementById(data.main.apps[i].route_name));
+                        $('#'+data.main.apps[i]['route_name']).prependTo('#app-list');
+                    }
+                }  
             }
         });
-    }
-    );
+    });
 }
 
 
