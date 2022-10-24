a78c6cafffd39da11a7d1ca7e27672e54595873b
Zayd Alzein <zaydalzein@gmail.com>
2022-10-24 16:48:27 -0500

    fix sw

--- public/script.js
+++ public/script.js
@@ -1,21 +1,14 @@
 // Register the service worker
-if ('serviceWorker' in navigator) {
-    // Wait for the 'load' event to not block other work
-    window.addEventListener('load', async () => {
-      // Try to register the service worker.
-      try {
-        // Capture the registration for later use, if needed
-        let reg;
-
-        // In production, use the normal service worker registration
-        reg = await navigator.serviceWorker.register('service-worker.js');
-  
-        console.log('Service worker registered! 😎', reg);
-      } catch (err) {
-        console.log('😥 Service worker registration failed: ', err);
-      }
-    });
-  }
+if(navigator.serviceWorker){
+    window.addEventListener('load',() => {
+      navigator.serviceWorker
+      .register('service-worker.js')
+      .then(console.log('[ServiceWorker] Registered Successfully'))
+      .catch(err => console.log(`[ServiceWorker] Error: ${err}`));
+     });
+ } else {
+     console.log('Service Worker not supported.');
+ }
 
 let deferredPrompt;
 const addBtn = document.querySelector(".add-button");
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,55 +1,17 @@
-const PRECACHE = 'precache-v1.03';
-const RUNTIME = 'runtime';
+const cacheName='cache-2020.10.06-01';
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  '/' // Alias for index.html
-];
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(event.request);
+    const networkResponsePromise = fetch(event.request);
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
+    event.waitUntil(async function() {
+      const networkResponse = await networkResponsePromise;
+      await cache.put(event.request, networkResponse.clone());
+    }());
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+    // Returned the cached response if we have one, otherwise return the network response.
+    return cachedResponse || networkResponsePromise;
+  }());
 });
