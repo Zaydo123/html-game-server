6cb3322983d58af904e63150436bf58d5d8626c2
Zayd Alzein <zaydalzein@gmail.com>
2023-08-08 21:51:21 -0500

    ui fixes

--- public/search-for-games.js
+++ public/search-for-games.js
@@ -1,3 +1,37 @@
+//set search-bar opacity to 0 then when clicked slowly increase opacity to 1
+document.getElementById('search-bar').style.opacity = 0;
+
+let opacity = 0;
+
+function wait(){
+    setTimeout(function(){
+        opacity += 0.1;
+        document.getElementById('search-bar').style.opacity = opacity;
+        if(opacity < 1){
+            wait();
+        }
+    }, 50);
+}
+
+
+document.getElementById('search-icon').addEventListener('click', function(){
+
+    if(opacity == 0){
+        //add show class to search bar
+        document.getElementById('search-bar').classList.remove('hidden');
+        wait();
+    } else {
+        opacity = 0;
+        document.getElementById('search-bar').style.opacity = 0;
+        document.getElementById('search-bar').classList.add('hidden');
+    }
+
+
+});
+
+
+
+
 function searchGames(){
     let allCards = document.getElementsByClassName("game-card");
     let searchQuery = document.getElementById('search-bar');
